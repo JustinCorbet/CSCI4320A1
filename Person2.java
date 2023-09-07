38642d6a19899863be165419cbade1aeafb4d570
ajohnbellot <johnbellotalexia@gmail.com>
2023-09-06 22:42:19 -0400

    take the string input and return its characters in random order

---
+++ Person2.java
@@ -0,0 +1,63 @@
+package edu.clayton.csit.antlab.person;
+import java.util.Random;
+/**
+ *  A simple class for person 2
+ *  returns their name and a
+ *  modified string 
+ *
+ * @author Qu
+ * @version 1.1
+ */
+public class Person2 {
+    /** Holds the persons real name */
+    private String name;
+	 	/**
+	 * The constructor, takes in the persons
+	 * name
+	 * @param pname the person's real name
+	 */
+	 public Person2(String pname) {
+	   name = pname;
+	 }
+	/**
+	 * This method should take the string
+	 * input and return its characters in
+	 * random order.
+	 * given "gtg123b" it should return
+	 * something like "g3tb1g2".
+	 *
+	 * @param input the string to be modified
+	 * @return the modified string
+	 */
+	private String calc(String input) {
+	  //Person 2 put your implementation here
+		//for i from n - 1 downto 1 do
+			//k = random integer with 0 <= k <=i
+			//exchange a[k] and a[i]
+		StringBuilder randomizer = new StringBuilder(input);
+
+		for (int i = randomizer.length() - 1; i > 0; i--)
+		{
+			int k = new Random().nextInt(i + 1);
+
+			char tmp = randomizer.charAt(i);
+			randomizer.setCharAt(i, randomizer.charAt(k));
+			randomizer.setCharAt(k, tmp);
+		}
+
+		return randomizer.toString();
+
+	  return null;
+	}
+	/**
+	 * Return a string rep of this object
+	 * that varies with an input string
+	 *
+	 * @param input the varying string
+	 * @return the string representing the 
+	 *         object
+	 */
+	public String toString(String input) {
+	  return name + calc(input);
+	}
+}
